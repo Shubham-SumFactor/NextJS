b8c6e42b427def08716b5e0ed59210da4b8ebd0b
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-02 12:08:59 +0530

    Aboutus page added with module.css

--- find-coffee-stores/pages/aboutus.js
+++ find-coffee-stores/pages/aboutus.js
@@ -1,6 +1,8 @@
+import styles from '@/styles/Aboutus.module.css';
+
 const Aboutus = () =>{
-    return <div>
-        <p>Contact us at our Social Media Handles</p>
+    return <div className ={styles.container}>
+        <h1><p>Contact us at our Social Media Handles</p></h1>
     </div>
 }
 export default Aboutus;
