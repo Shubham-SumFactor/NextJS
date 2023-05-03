b994faf48ab286430367435e5d9ccf08e7643321
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-03 14:39:06 +0530

    dynamic routing for coffee store

--- find-coffee-stores/pages/coffee-store/[id].js
+++ find-coffee-stores/pages/coffee-store/[id].js
@@ -1,11 +1,24 @@
 import { useRouter } from "next/router";
+import Link from "next/link"
 
 const CoffeeStore = () =>{
     const router = useRouter();
     console.log("router", router); 
 
-    return <div>Coffee Store Page {router.query.id}</div>
-
+    return <div>Coffee Store Page {router.query.id}
+        
+    <Link href ="/">  
+   Back To home  
+    </Link>
+    <br></br>
+    
+    <Link href ="/coffee-store/dynamic">  
+   Go To Dynamic Page
+    </Link>
+    
+    
+    </div>
+    
 };
 
 export default CoffeeStore;
