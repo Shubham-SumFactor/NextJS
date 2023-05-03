909b9698238ec793f388191c2b497be0806d3e16
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-03 14:36:59 +0530

    banner updated

--- find-coffee-stores/components/banner.js
+++ find-coffee-stores/components/banner.js
@@ -10,8 +10,7 @@ const Banner = (props) => {
             </h1>
         <p className={styles.subTitle}>Discover your local coffee shops!</p>
         
-        <button className={styles.button} onClick={props.handleOnClick}
-        >{props.buttonText} 
+        <button className={styles.button} onClick={props.handleOnClick} >{props.buttonText} 
         </button>
     
     
