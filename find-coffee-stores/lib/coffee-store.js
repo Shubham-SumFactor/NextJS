9335e93ffba2de709d2466b962683487f5c96805
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-15 17:22:02 +0530

    finding stores nearby , recieving lat and long updated

--- find-coffee-stores/lib/coffee-store.js
+++ find-coffee-stores/lib/coffee-store.js
@@ -2,40 +2,40 @@ import { createApi } from 'unsplash-js';
 //import nodeFetch from 'node-fetch';
 
 const unsplash = createApi({
-  accessKey: process.env.UNSPLASH_API_KEY,
+  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_API_KEY,
   
 });
 
 
-const getUrlForCoffeeStores = (latlong, query, limit) =>{
-    return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${latlong}&limit=${limit}`
+const getUrlForCoffeeStores = (latLong, query, limit) =>{
+    return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${latLong}&limit=${limit}`
 }
 
 const getListOfCoffeeStoresPhotos = async () => {
 
   const photos= await unsplash.search.getPhotos({
     query: 'coffee shop',
-    page: 1,
+  //  page: 1,
     perPage: 30,
 
   });
-  const unsplashResults = photos.response.results
+  const unsplashResults = photos.response.results;
   
   return unsplashResults.map((result) => result.urls["small"]);
 }
 
-export const fetchCoffeeStores = async () => {
+export const fetchCoffeeStores = async (latLong="43.66648830062341%2C-79.41309107123831", limit=6) => {
   const photos = await getListOfCoffeeStoresPhotos();
   
     const options = {
         method: 'GET',
         headers: {
           accept: 'application/json',
-          Authorization: process.env.FOURSQUARE_API_KEY
+          Authorization: process.env.NEXT_PUBLIC_FOURSQUARE_API_KEY,
         },
       };
       
-      const response = await fetch(getUrlForCoffeeStores("43.66648830062341%2C-79.41309107123831","coffee", 6 ), options);
+      const response = await fetch(getUrlForCoffeeStores(latLong,"coffee", limit ), options);
       
       const data = await response.json();
       return data.results.map((result, idx) =>{
